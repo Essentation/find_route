4e4ccf82a89874b45ee05ac7d9d3c6edb20f4e9f
Essentation <nikitasikrieru@gmail.com>
2023-11-01 02:56:12 +0300

    работа с русификацией и тд

--- cities/models.py
+++ cities/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name='Город')
+
+    def __str__(self):      #Отображение в админке названия города
+        return self.name
+
+    class Meta:     #Отображение вместо названия таблицы City
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']         #Отображение городов по алфавиту
