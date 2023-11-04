d83c0f2b0ac2ac77053f0155eda53252402e211f
Essentation <nikitasikrieru@gmail.com>
2023-11-04 21:01:32 +0300

    Добавил отображение списка найденных маршрутов

--- routes/utils.py
+++ routes/utils.py
@@ -47,7 +47,7 @@ def get_routes(request, form) -> dict:
             raise ValueError('Маршрут через эти города невозможен')
     else:
         right_ways = all_ways
-    trains = []
+    routes = []
     all_trains = {}
     for q in qs:
         all_trains.setdefault((q.from_city_id, q.to_city_id), [])
@@ -60,10 +60,26 @@ def get_routes(request, form) -> dict:
             qs = all_trains[(route[i], route[i + 1])]
             q = qs[0]
             total_time += q.travel_time
-            tmp['trains'].append(qs)
+            tmp['trains'].append(q)
         tmp['total_time'] = total_time
         if total_time <= travelling_time:
-            trains.append(tmp)
-    if not trains:
+            # Если общее время в пути меньше заданного,
+            # то добавляем маршрут в общий список
+            routes.append(tmp)
+    if not routes:
+            # Если список пуст, то нет таких маршрутов,
+            # которые удовлетворяли бы заданным условиям
         raise ValueError('Время в пути больше заданного')
+    sorted_routes = []
+    if len(routes) == 1:
+        sorted_routes = routes
+    else:
+        times = list(set(r['total_time'] for r in routes))
+        times = sorted(times)
+        for time in times:
+            for route in routes:
+                if time == route['total_time']:
+                    sorted_routes.append(route)
+    context['routes'] = sorted_routes
+    context['cities'] = {'from_city': from_city.name, 'to_city': to_city.name}
     return context
