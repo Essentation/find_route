f62ef0e2b5972af9cd987b3be2f56e3a242289de
Essentation <nikitasikrieru@gmail.com>
2023-11-04 10:46:30 +0300

    Добавил функцию по поиску путей в графе

--- routes/utils.py
+++ routes/utils.py
@@ -1,7 +1,23 @@
 from trains.models import Train
 
 
-def get_graph(qs):
+def dfs_paths(graph, start, goal):
+    """Функция поиска всех возможных маршрутов
+    из одного города в другой. Вариант посещения
+    одного и того же города более одного раза
+    не рассматривается."""
+    stack = [(start, [start])]
+    while stack:
+        (vertex, path) = stack.pop()
+        if vertex in graph.keys():
+            for next_ in graph[vertex] - set(path):
+                if next_ == goal:
+                    yield path + [next_]
+                else:
+                    stack.append((next_, path + [next_]))
+
+
+def get_graph(qs):  # функция формирования графа
     graph = {}
     for q in qs:
         graph.setdefault(q.from_city_id, set())
@@ -10,8 +26,16 @@ def get_graph(qs):
 
 
 def get_routes(request, form) -> dict:
+    context = {'form': form}
     qs = Train.objects.all()
     graph = get_graph(qs)
-
+    data = form.cleaned_data
+    from_city = data['from_city']
+    to_city = data['to_city']
+    travelling_time = data['travelling_time']
+    all_ways = dfs_paths(graph, from_city.id, to_city.id)
+    if not len(list(all_ways)):
+        raise ValueError('Маршрута, удовлетворяющего условиям, не существует')
+    return context
 
 
