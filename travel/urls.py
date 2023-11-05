85ff754f8cc22f0c1d24fc7009ecd7e1def7aa33
Essentation <nikitasikrieru@gmail.com>
2023-11-05 19:22:33 +0300

    Добавил детализацию маршрутов

--- travel/urls.py
+++ travel/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from routes.views import (
-    home, find_routes, add_route, save_route, RouteListView,
+    home, find_routes, add_route, save_route, RouteListView, RouteDetailView,
 )
 
 urlpatterns = [
@@ -30,4 +30,5 @@ urlpatterns = [
     path('add_route/', add_route, name='add_route'),
     path('save_route/', save_route, name='save_route'),
     path('list/', RouteListView.as_view(), name='list'),
+    path('detail/<int:pk>/', RouteDetailView.as_view(), name='detail'),
 ]
