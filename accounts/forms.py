f8dde4e06fce35e53cfa17ad5366f35a9227ebda
Essentation <nikitasikrieru@gmail.com>
2023-11-06 05:49:38 +0300

    Добавил функции входа-выхода пользователей

--- accounts/forms.py
+++ accounts/forms.py
@@ -17,16 +17,16 @@ class UserLoginForm(forms.Form):
             'placeholder': 'Введите password'
         }))
 
-        def clean(self, *args, **kwargs):
-            username = self.cleaned_data.get('username')
-            password = self.cleaned_data.get('password')
-            if username and password:
-                qs = User.objects.filter(username=username)
-                if not qs.exists():
-                    raise forms.ValidationError('Такого пользователя нет')
-                if not check_password(password, qs[0].password): # Если пользователь ввёл правильный пароль, то он будет зашифрован и сравнится с тем, который хранится в базе данных
-                    raise forms.ValidationError('Неверный пароль')
-                user = authenticate(username=username, password=password)
-                if not user:
-                    raise forms.ValidationError('Данный пользователь неактивен')
-            return super().clean(*args, **kwargs)
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        if username and password:
+            qs = User.objects.filter(username=username)
+            if not qs.exists():
+                raise forms.ValidationError('Такого пользователя нет')
+            if not check_password(password, qs[0].password): # Если пользователь ввёл правильный пароль, то он будет зашифрован и сравнится с тем, который хранится в базе данных
+                raise forms.ValidationError('Неверный пароль')
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise forms.ValidationError('Данный пользователь неактивен')
+        return super().clean(*args, **kwargs)
